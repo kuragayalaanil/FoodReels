d884709932ffc57cec95cf1db64a22f911cd0516
Anil Kumar <kuragayalaanil007@gmail.com>
2025-12-10 15:08:01 +0530

    Register Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserRegister from "./pages/auth/userRegister";
+
+const App = () => {
+  return (
+    <div>
+      <UserRegister />
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/src/pages/auth/UserLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>userLogin</div>
+  )
+}
+
+export default UserLogin
